5b7cfdd13c3b685ecfab2c080f1939db4e37371f
giorgio-hash <g.chirico1@studenti.unibg.it>
2024-04-22 21:15:58 +0200

    aggiunto MessageBroker e NotificaOrdineDTO
    modificato application.properties e application.yml
    
    aggiunto mapper da OrdineEntity a NotificaOrdineDTO
    
    implementata messagePort e testata tramite Postman
    
    piccola modifica allo schema del DB: timestamp ora è "NOT NULL"

--- src/main/java/com/example/gestionecliente/Domain/MessagePort.java
+++ src/main/java/com/example/gestionecliente/Domain/MessagePort.java
@@ -1,5 +1,7 @@
 package com.example.gestionecliente.Domain;
 
+import com.example.gestionecliente.Domain.dto.NotificaOrdineDTO;
+
 public interface MessagePort {
-    void sendOrder();
+    void sendOrder(NotificaOrdineDTO notificaOrdineDTO);
 }
